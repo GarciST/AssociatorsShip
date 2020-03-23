75eef4e3c1b5cef89071c469c0cb94c1876fd5f5
Santiago Garcia Diaz <Santiago.Garcia.Diaz@unit4.com>
2020-03-23 12:08:03 +0100

    First true prototype

--- src/associators/ship/FirstShip.java
+++ src/associators/ship/FirstShip.java
@@ -1,17 +1,65 @@
 package associators.ship;
 
+import java.awt.Color;
+
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
 import robocode.ScannedShipEvent;
 import robocode.Ship;
+import robocode.util.Utils;
 
 public class FirstShip extends Ship{
 	
+	private boolean direction = true;
+	
 	public void run(){ 
-		 setAhead(200);  
-		 setTurnRightDegrees(90);
-		 setBack(19.0);
+		setTurnRadarRightDegrees(Double.POSITIVE_INFINITY);
+		
+		//while(true) {
+			
+		//}
     }
+	
+	public void onHitWall(HitWallEvent event){
+		direction = !direction;
+		setTurnRightDegrees(45);
+	}
+
+	public void onHitRobot(HitRobotEvent event){
+		//Fill in something you'd like your Ship to do when it hits another Ship
+	}
  
-    public void onScannedShip(ScannedShipEvent e){
-        fireFrontCannon(1);
+    public void onScannedShip(ScannedShipEvent event){
+    	
+    	fixRadar(event);
+        
+        setTurnFrontCannonRightRadians(event.getBearingFrontRadians());
+        setTurnBackCannonRightRadians(event.getBearingBackRadians());
+        
+		while(getFrontCannonTurnRemainingRadians() != 0 || getBackCannonTurnRemainingRadians() != 0){
+			execute();
+			if(getFrontCannonAtBlindSpot() || getBackCannonAtBlindSpot())
+				break;
+		}
+		
+		if(!getFrontCannonAtBlindSpot()){
+			fireFrontCannon(2);
+			setAhead(150);
+		}
+		
+		if(!getBackCannonAtBlindSpot()){
+			fireBackCannon(2);
+			setBack(150);
+		}
+    }
+    
+    private void fixRadar(ScannedShipEvent event) {
+    	double radarTurn =
+    	        // Absolute bearing to target
+    	        getBodyHeadingRadians() + event.getBearingRadians()
+    	        // Subtract current radar heading to get turn required
+    	        - getRadarHeadingRadians();
+    	 
+    	    setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
     }
 }
